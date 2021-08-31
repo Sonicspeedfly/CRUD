1ec560b4f9481fed6dc479bcacb5312ca36b5d40
Sonicspeedfly <sonicspeedfly@gmail.com>
2021-08-31 18:12:35 +0300

    v1, список и добавление постов

---
+++ main.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const http = require('http');
+const statusNotFound = 404;
+const statusBadRequest = 400;
+const statusOk = 200;
+
+let nextId = 1;
+const posts = [];
+
+function sendResponse(response, {status = statusOk, headers = {}, body = null}) {
+    Object.entries(headers).forEach(function([key, value]) {
+        response.setHeader(key, value);
+    });
+    response.writeHead(status);
+    response.end(body);
+}
+
+function sendJSON(response, body) {
+    sendResponse(response, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+    });
+}
+
+const methods = new Map();
+methods.set('/posts.get', function({response}) {
+    sendJSON(response, posts);
+});
+methods.set('/posts.getById', function() {});
+methods.set('/posts.post', function({response, searchParams}) {
+    if (!searchParams.has('content')) {
+        sendResponse(response, {status: statusBadRequest});
+        return;
+    }
+
+    const content = searchParams.get('content');
+
+    const post = {
+        id: nextId++,
+        content: content,
+        created: Date.now(),
+    };
+
+    posts.unshift(post);
+    sendJSON(response, post);
+});
+methods.set('/posts.edit', function(request, response) {});
+methods.set('/posts.delete', function(request, response) {});
+
+const server = http.createServer((request, response) => {
+    const {pathname, searchParams} = new URL(request.url, `http://${request.headers.host}`);
+    
+    const method = methods.get(pathname);
+    if (method === undefined) {
+        sendResponse(response, {status: statusNotFound})
+        return;
+    }
+
+    const params = {
+        request,
+        response,
+        pathname,
+        searchParams,
+    };
+
+    method(params);
+});
+
+const port = 9999;
+server.listen(port); 
